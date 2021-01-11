e1b75aafa11839ccf69681d1a79ae673798ea5a1
haiyang.a.zhao <haiyang.a.zhao@nokia-sbell.com>
2021-01-11 15:22:49 +0800

    entry

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,15 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: {
+        import: './src/index.js',
+        dependOn: 'shared',
+    },
+    another: {
+        import: './src/another-module.js',
+        dependOn: 'shared',
+    },
+    shared: 'lodash',
   },
   devtool: 'inline-source-map',
   devServer: {
