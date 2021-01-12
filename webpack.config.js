beba42207be191fd0452e6fb19607a82237b88f7
haiyang.a.zhao <haiyang.a.zhao@nokia-sbell.com>
2021-01-12 08:53:47 +0800

    SplitChunksPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,31 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: {
-        import: './src/index.js',
-        dependOn: 'shared',
-    },
-    another: {
-        import: './src/another-module.js',
-        dependOn: 'shared',
-    },
-    shared: 'lodash',
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+  // devtool: 'inline-source-map',
+  // devServer: {
+  //   contentBase: './dist',
+  // },
+  // plugins: [
+  //   new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  //   new HtmlWebpackPlugin({
+  //     title: 'Development',
+  //   }),
+  // ],
   output: {
     filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
       publicPath: '/',
    },
+   optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
  };
